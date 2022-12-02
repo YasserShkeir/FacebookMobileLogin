6b02abdadafce62e15673daca8516075fd8299da
Yasser Shkeir <shkeiryasser@gmail.com>
2022-12-03 01:50:38 +0200

    Frontend: Profile-> Screen Implementation

---
+++ src/profile.js
@@ -0,0 +1,70 @@
+import { useEffect } from "react";
+import {
+  Alert,
+  BackHandler,
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+const Profile = ({ route, navigation }) => {
+  const { user } = route.params;
+
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("Hold on!", "Are you sure you want to exit the app?", [
+        {
+          text: "Cancel",
+          onPress: () => null,
+          style: "cancel",
+        },
+        { text: "YES", onPress: () => BackHandler.exitApp() },
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
+  return (
+    <View style={styles.profile}>
+      <Image source={{ uri: user.picture.data.url }} style={styles.image} />
+      <Text style={styles.name}>{user.name}</Text>
+      <Text>ID: {user.id}</Text>
+      <Button
+        title="Logout"
+        onPress={() => {
+          navigation.navigate("Login");
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profile: {
+    alignItems: "center",
+  },
+  name: {
+    fontSize: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+});
+
+export default Profile;
