347f0210debaaae1bc27cc2f5ef1b78eb37ab476
Yasser Shkeir <shkeiryasser@gmail.com>
2022-12-03 17:53:43 +0200

    Backend: Index-> Add Routes to App

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
+const authRoutes = require("./route/auth.routes");
+app.use("/auth", authRoutes);
+
+const userRoutes = require("./route/user.routes");
+app.use("/user", userRoutes);
+
 app.listen(process.env.PORT, (err) => {
   if (err) throw err;
   console.log(`Server is running in Port: ${process.env.PORT}`);
