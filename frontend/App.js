11f3f869980771349978a233dc4f9d58cb04a65d
Yasser Shkeir <shkeiryasser@gmail.com>
2022-12-03 17:51:45 +0200

    Frontend: App-> Main Screens Finalized

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,16 @@
 import "react-native-gesture-handler";
+import { LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Components
-import Login from "./src/login";
-import DrawerNavigator from "./src/drawerNav";
+import Login from "./src/screens/login";
+import Signup from "./src/screens/signup";
+import DrawerNavigator from "./src/components/drawerNav";
 
 const Stack = createNativeStackNavigator();
+
+LogBox.ignoreAllLogs();
 
 export default function App() {
   return (
@@ -19,6 +23,7 @@ export default function App() {
             headerShown: false,
           }}
         />
+        <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen
           name="DrawerNavigator"
           component={DrawerNavigator}
