6cc88cd0447832b0a424e9164a3f930d214605e6
Yasser Shkeir <shkeiryasser@gmail.com>
2022-12-05 00:14:32 +0200

    Frontend: App-> Add Apollo Provider

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,8 @@
 import "react-native-gesture-handler";
-import { LogBox } from "react-native";
+import { LogBox, AppRegistry } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // Components
 import Login from "./src/screens/login";
@@ -12,26 +13,38 @@ const Stack = createNativeStackNavigator();
 
 LogBox.ignoreAllLogs();
 
+// Apollo Client Setup
+// Using the 10.0.2.2 as we are using an emulator, replace with your IP address if you are using a physical device
+const client = new ApolloClient({
+  uri: `http://10.0.2.2:3000/graphql`,
+  cache: new InMemoryCache(),
+  defaultOptions: { watchQuery: { fetchPolicy: "cache-and-network" } },
+});
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen
-          name="DrawerNavigator"
-          component={DrawerNavigator}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen
+            name="DrawerNavigator"
+            component={DrawerNavigator}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
+
+AppRegistry.registerComponent("nadeeraV3", () => App);
