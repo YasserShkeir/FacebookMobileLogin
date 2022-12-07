7e61423efdc2841fda0474e899ad084691c63706
Yasser Shkeir <shkeiryasser@gmail.com>
2022-12-07 19:22:19 +0200

    Backend: Structure-> Delete pre-GraphQL Routes

--- server/index.js
+++ server/index.js
@@ -17,12 +17,6 @@ app.use(
   })
 );
 
-const authRoutes = require("./route/auth.routes");
-app.use("/auth", authRoutes);
-
-const userRoutes = require("./route/user.routes");
-app.use("/user", userRoutes);
-
 app.listen(process.env.PORT, (err) => {
   if (err) throw err;
   console.log(`Server is running in Port: ${process.env.PORT}`);
