e9ae57ff2384d4d0589143583151ca7b335c08d2
Yasser Shkeir <shkeiryasser@gmail.com>
2022-12-07 19:18:29 +0200

    Backend: Structure-> Delete pre-GraphQL Files

--- server/route/user.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { getSelf } = require("../controller/user.controller");
-
-const router = Router();
-
-router.post("/self", getSelf);
-
-module.exports = router;
